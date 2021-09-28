efeaa78480220b0c876f4274d55cd47d8a496b23
jkim333 <kimjihyung3@gmail.com>
2021-09-28 11:51:10 +1000

    Modified main.rb to accept -h command line argument.

--- src/main.rb
+++ src/main.rb
@@ -1,5 +1,24 @@
-require_relative 'quiz'
+require_relative "quiz"
+require 'colorize'
 
 quiz = Quiz::Quiz.new
 
-quiz.run(nil)
+input_array = ARGV
+
+if input_array.include?("-h")
+    quiz.clear_terminal
+    puts "Welcome to Trivia Quiz game!".colorize(:light_blue)
+    puts ""
+    puts "This game create a fun trivia quiz games for you.".colorize(:light_green)
+    puts ""
+    puts "Here is a brief summary of how the game works:"
+    puts "1. The application will ask you the following questions: your nickname, the number of questions you would like to play, the quiz category, and the level of difficulty."
+    puts "2. The application will then prepare the questions and present the questions to you and ask you to enter in your answer."
+    puts "3. After completing all of the questions, you will be notified of your score. You will be provided with three options to choose from: (1) review all answers, (2) play again, and (3) quit the game."
+    puts ""
+    puts "We hope that you find this instruction helpful :)".colorize(:light_yellow)
+    puts "Enjoy the game!".colorize(:light_yellow)
+    puts ""
+else
+    quiz.run(nil)
+end
